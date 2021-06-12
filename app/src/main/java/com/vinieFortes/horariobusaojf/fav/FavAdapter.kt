5b80fa3ab1a4c568811acbebbde872b3e2ef14a4
VinieFortes <vinie4apps@gmail.com>
2021-06-12 20:48:13 -0300

    Commit

---
+++ app/src/main/java/com/vinieFortes/horariobusaojf/fav/FavAdapter.kt
@@ -0,0 +1,65 @@
+package com.vinieFortes.horariobusaojf.fav
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.vinieFortes.horariobusaojf.R
+import java.util.ArrayList
+
+
+class MyRecyclerViewAdapter internal constructor(context: Context?, data: ArrayList<String>) :
+    RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder>() {
+    private val mData: ArrayList<String> = data
+    private val mInflater: LayoutInflater = LayoutInflater.from(context)
+    private var mClickListener: ItemClickListener? = null
+    private lateinit var context: Context
+
+    // inflates the cell layout from xml when needed
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = mInflater.inflate(R.layout.layout_main, parent, false)
+        return ViewHolder(view)
+    }
+
+    // binds the data to the TextView in each cell
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.myTextView.text = mData[position]
+    }
+
+    // total number of cells
+    override fun getItemCount(): Int {
+        return mData.size
+    }
+
+    // stores and recycles views as they are scrolled off screen
+    inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
+        var myTextView: TextView = itemView.findViewById(R.id.linha)
+        override fun onClick(view: View) {
+            if (mClickListener != null) mClickListener!!.onItemClick(view, adapterPosition)
+        }
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+    }
+
+    // convenience method for getting data at click position
+    fun getItem(id: Int): String {
+        return mData[id]
+    }
+
+    // allows clicks events to be caught
+    fun setClickListener(itemClickListener: Favoritos) {
+        mClickListener = itemClickListener
+    }
+
+    // parent activity will implement this method to respond to click events
+    interface ItemClickListener {
+        fun onItemClick(view: View?, position: Int)
+    }
+
+}
---
+++ app/src/main/java/com/vinieFortes/horariobusaojf/fav/Item Class.kt
@@ -0,0 +1,6 @@
+package com.vinieFortes.horariobusaojf.fav
+
+internal class MyItem {
+    var name: String? = null
+    var number: String? = null
+}
